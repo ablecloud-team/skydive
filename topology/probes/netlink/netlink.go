ce702552c518ce21c48935334f92f228dcf0ee1f
stardom364 <stardom364@gmail.com>
2021-08-23 16:49:58 +0900

    [Bug Fixed] in  FUNC "onNeighborsChanged"

--- topology/probes/netlink/netlink.go
+++ topology/probes/netlink/netlink.go
@@ -862,10 +862,6 @@ func (u *Probe) onNeighborsChanged(index int64, fdb *topology.Neighbors, neighbo
 	intf := u.Ctx.Graph.LookupFirstChild(u.Ctx.RootNode, graph.Metadata{"IfIndex": index})
 
 	if intf == nil {
-		u.initialize()
-		intf = u.Ctx.Graph.LookupFirstChild(u.Ctx.RootNode, graph.Metadata{"IfIndex": index})
-	}
-	if intf == nil {
 		if _, err := u.handle.LinkByIndex(int(index)); err == nil {
 			u.Ctx.Logger.Errorf("Unable to find interface with index %d to add a new neighbors", index)
 		}
